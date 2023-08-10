43b5285735d0faaca5799a1cb7554d5e1921b0db
Gabriel Antonio da Silva <gabriel.uag.ufrpe@gmail.com>
2023-08-10 18:57:21 -0300

    servidor inicial com express

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import express from "express";
+import db from "./config/dbConnect.js";
+
+
+
+export default app;
---
+++ src/config/dbConnect.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+
+let db = mongoose.connection;
+
+export default db;
